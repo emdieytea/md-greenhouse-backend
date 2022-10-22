5fbd052775b38faa4d325425c8d589bbe2fe7b5a
DESKTOP-V8HD01C\eMDi <md@mvsoftech.ph>
2022-10-22 19:02:16 +0800

    Added username to users table Added seeder for admin

---
+++ database/seeders/AdminSeeder.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Carbon\Carbon;
+use Hash;
+use Illuminate\Database\Seeder;
+
+class AdminSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        // Create admin 1
+        User::updateOrCreate(
+            [
+                'name' => 'Greenhouse Admin',
+                'username' => 'greenhouse-admin',
+                'email' => 'greenhouse-admin@emdieytea.com',
+            ],
+            [
+                'email_verified_at' => null, // Carbon::now()->toDateTimeString(),
+                'password' => Hash::make('password'),
+            ]
+        );
+    }
+}
