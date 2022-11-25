741d4547ab7dce28eeb45cac05acf5c0e08d311c
DESKTOP-V8HD01C\eMDi <md@mvsoftech.ph>
2022-11-26 00:59:53 +0800

    Added check batch status command which run every minute

---
+++ app/Console/Commands/CheckBatchStatusCommand.php
@@ -0,0 +1,79 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Models\Node;
+use Carbon\Carbon;
+use Illuminate\Console\Command;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Http;
+
+class CheckBatchStatusCommand extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'command:check_batch_status';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Checks the status of the batches on the web.';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return int
+     */
+    public function handle()
+    {
+        $this->info('[' . Carbon::now()->format('Y-m-d H:i:s') . '] Executing checking of status of the batches on the web!');
+
+        try {
+            DB::beginTransaction();
+            
+            $date_now = Carbon::now();
+
+            $nodes = Node::where('status', 1)->get();
+
+            if ($nodes->isNotEmpty()) {
+                foreach ($nodes as $key => $node) {
+                    if ($date_now->diffInMinutes($node->updated_at) > 3) {
+                        $node->status = 0;
+                        $node->save();
+
+                        $this->info('[' . Carbon::now()->format('Y-m-d H:i:s') . '] Batch # ' . $node->batch_no . ' was checked and deemed offline.');
+                    } else {
+                        $this->info('[' . Carbon::now()->format('Y-m-d H:i:s') . '] Batch # ' . $node->batch_no . ' was checked and is still online.');
+                    }
+                }
+
+                DB::commit();
+
+                $this->info('[' . Carbon::now()->format('Y-m-d H:i:s') . '] Status of the batches on the web has been checked successfully.');
+                return 200;
+            }
+
+            $this->error('[' . Carbon::now()->format('Y-m-d H:i:s') . '] Failed to check, please try again!');
+            return 400;
+        } catch (\Exception $e) {
+            DB::rollBack();
+            $this->error('[' . Carbon::now()->format('Y-m-d H:i:s') . '] Something went wrong please try again!'); // $e->getMessage()
+            return 400;
+        }
+    }
+}
--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -2,6 +2,7 @@
 
 namespace App\Console;
 
+use Carbon\Carbon;
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
 
@@ -16,6 +17,20 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule)
     {
         // $schedule->command('inspire')->hourly();
+        
+        $date = Carbon::now()->format('Y-m-d');
+        
+        // $schedule
+        //     ->command('command:seed_data')
+        //     ->hourly();
+        //     // ->appendOutputTo(storage_path('logs/seed-' . $date . '.log'))
+        //     // ->withoutOverlapping();
+
+        $schedule
+            ->command('command:check_batch_status')
+            ->everyMinute()
+            ->appendOutputTo(storage_path('logs/check-status-' . $date . '.log'))
+            ->withoutOverlapping();
     }
 
     /**
